00967b32d9798bcbae007dce8a5e89cfb8c6d449
ShivanshiModi <87198158+ShivanshiModi@users.noreply.github.com>
2022-08-28 17:54:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Prediction="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function gestures()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='capture' src='"+data_uri+"'/>";
+
+    });
+}
+console.log('ml5 version', ml5.version);
+classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/TVQ80hcsW/model.json', modelloaded());
+
+function modelloaded()
+{
+    console.log('model is loaded');
+}
+
+function speak()
+{
+    var synth=window.speechSynthesis;
+    s ="The prediction is " + Prediction;
+    var utterthis = new SpeechSynthesisUtterance(s);
+    synth.speak(utterthis);
+}
